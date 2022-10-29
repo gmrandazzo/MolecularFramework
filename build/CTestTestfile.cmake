1e10cbeada42f6dc19ebd02ba97221ed4e738cb7
Giuseppe Marco Randazzo <gmrandazzo@gmail.com>
2022-10-29 07:01:48 +0200

    Cleanup

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marco/Nextcloud/Software/MolecularFramework
-# Build directory: /home/marco/Nextcloud/Software/MolecularFramework/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
--- build/src/bin_src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marco/Nextcloud/Software/MolecularFramework/src/bin_src
-# Build directory: /home/marco/Nextcloud/Software/MolecularFramework/build/src/bin_src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
